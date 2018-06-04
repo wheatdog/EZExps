04156fa976c33eeef7d66614fbc7073c4c570e9f
wheatdog <wheatdoge@gmail.com>
2018-06-04 17:13:24 +0800

    Use google-auth

--- gsheet_uploader.py
+++ gsheet_uploader.py
@@ -1,7 +1,8 @@
 import gspread
 from pymongo import MongoClient
 
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
 from bson.objectid import ObjectId
 
 AUTH_JSON_PATH='private/auth.json'
@@ -10,22 +11,26 @@ DATABASE='crossdata-seg'
 
 
 def auth_gss_client(path, scopes):
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
-    return gspread.authorize(credentials)
+    credentials = service_account.Credentials.from_service_account_file(
+                path)
+    scoped_credentials = credentials.with_scopes(scopes)
+    gss_client = gspread.Client(auth=scoped_credentials)
+    gss_client.session = AuthorizedSession(scoped_credentials)
+    return gss_client
 
-def update_sheet(gss_client, key, exp_data):
+def update_sheet(gss_client, key, sheet_name, exp_data):
     wks = gss_client.open_by_key(key)
     exists_worksheets = [item.title for item in wks.worksheets()]
-    if DATABASE not in exists_worksheets:
-        wks.add_worksheet(DATABASE, 0, 0)
-    sheet = wks.worksheet(DATABASE)
+    if sheet_name not in exists_worksheets:
+        wks.add_worksheet(sheet_name, 0, 0)
+    sheet = wks.worksheet(sheet_name)
 
     upload_data = [
             str(exp_data['_id']),
             exp_data['time']['start'],
             exp_data['time']['end'],
             exp_data['time']['elapsed'],
-            #exp_data['purpose'],
+            exp_data['purpose'],
             str(exp_data['args']),
             str(exp_data['src'])
             ]
@@ -39,12 +44,13 @@ def main():
     client = MongoClient()
     db = client[DATABASE]
     collect = db['runs']
-    test_id='5b12cd0b1d41c87432d6bef5' 
+    test_id='5b14d5e01d41c88d0492623c'
     post = collect.find_one({'_id': ObjectId(test_id)})
 
+    sheet_name = '{}.archive'.format(DATABASE)
     with open(GSHEET_KEY) as f:
         skey = f.read().strip()
-        update_sheet(gss_client, skey, post)
+        update_sheet(gss_client, skey, sheet_name, post)
 
 if __name__ == '__main__':
     main()
